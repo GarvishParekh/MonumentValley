811164ece40275b958a2a59cb770fca48a6f46c8
Yash Panchal <yash.p@theintellify.com>
2024-06-05 18:52:35 +0530

    New level added with moving tile functionality

--- Assets/_MY ASSETS/Code/Behiviour/GroundFunction.cs
+++ Assets/_MY ASSETS/Code/Behiviour/GroundFunction.cs
@@ -1,12 +1,45 @@
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
+
+public enum GroundType
+{
+    Normal,
+    Moving,
+    Rotation
+}
+
+public enum MovingTileDirection
+{
+    X,
+    Y,
+    Z
+}
 
 public class GroundFunction : MonoBehaviour
 {
+    public GroundType groundType;
+    public MovingTileDirection tileDirection;
+    
     [Header("<size=15>[SCRIPTABLE OBJECT]")]
     [SerializeField] private PlayerData playerData;
 
+    [Header("<size=15>[COMPONENTS]")]
+    [SerializeField] private Transform movingTile;
+
+    [Header("<size=15>[VALUES]")]
+    [SerializeField] private float Degree = 0;
+    [SerializeField] private float waveSpeed = 1;
+    [SerializeField] private float waveHeight = 1;
+    [SerializeField] private float heightOffset = 0;
+    [SerializeField] private float rotationValue;
+    [SerializeField] private float rotationSpeed;
+
+    private Vector3 tilePositionVector;
     private float groundStartPosition;
-    
+    private float sineWave = 0;
+    private float cosWave = 0;
+
     private void OnEnable()
     {
         BallFunction.TrapActivate += GroundAnimation;
@@ -21,7 +54,26 @@ public class GroundFunction : MonoBehaviour
     {
         groundStartPosition = transform.localPosition.y;
     }
-    
+
+    private void Update()
+    {
+        switch (groundType)
+        {
+            case GroundType.Normal:
+                break;
+
+            case GroundType.Moving:
+                Debug.Log("Tile is moving");
+                    MovingGroundAnimation();
+                break;
+
+            case GroundType.Rotation:
+                groundRotateAroundAnimation();
+                break;
+
+        }
+    }
+
 
     private void GroundAnimation(Material _TrapMat)
     {
@@ -50,5 +102,53 @@ public class GroundFunction : MonoBehaviour
                 });
             });
         }); 
+    }
+
+    private void MovingGroundAnimation()
+    {
+        float tempSpeed = (Mathf.Abs(sineWave) + 0.2f) * waveSpeed; 
+        Degree += Time.deltaTime * tempSpeed;
+        sineWave = (Mathf.Sin(Degree) * waveHeight) + heightOffset;
+
+        switch (tileDirection)
+        {
+            case MovingTileDirection.X:
+                    tilePositionVector.x = sineWave;
+                break;
+
+            case MovingTileDirection.Y:
+                    tilePositionVector.y = sineWave;
+                break;
+
+            case MovingTileDirection.Z:
+                tilePositionVector.z = sineWave;  
+                break;
+        }
+        transform.localPosition = tilePositionVector;
+    }
+
+
+    float tempSpeed = 0;
+    private void groundRotateAroundAnimation()
+    {
+        if (cosWave < 0 && sineWave > 0)
+        {
+            tempSpeed = ((Mathf.Abs(cosWave) * Mathf.Abs(sineWave)) + 0.05f) * waveSpeed;
+        }
+        else
+        {
+            tempSpeed = waveSpeed;
+        }
+
+        tempSpeed = Mathf.Clamp(tempSpeed, 0, waveSpeed);
+        Degree += Time.deltaTime * tempSpeed;
+
+        sineWave = (Mathf.Sin(Degree) * waveHeight) + heightOffset;
+        cosWave = (Mathf.Cos(Degree) * waveHeight) + heightOffset;
+
+        tilePositionVector.x = cosWave;
+        tilePositionVector.z = sineWave;
+
+        transform.localPosition = tilePositionVector;
     }
 }
