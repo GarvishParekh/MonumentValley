12a857d34c613e47269558358795df5ba5ab1777
Garvish Parekh <garvish.p@theintellify.com>
2024-06-25 19:52:16 +0530

    Level selection and star collection completed

--- Assets/_MY ASSETS/Code/Behiviour/LevelBtn.cs
+++ Assets/_MY ASSETS/Code/Behiviour/LevelBtn.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class LevelBtn : MonoBehaviour
 {
+    [SerializeField] private GameObject lockImage;
+    [SerializeField] private GameObject currentLevelImage;
+
     public int levelIndex;
     public TMP_Text levelIndexTxt;
     
@@ -20,6 +23,12 @@ public class LevelBtn : MonoBehaviour
         myButton.onClick.AddListener(SetLevelIndex);
         int tempIndex = levelIndex + 1;
         levelIndexTxt.text = tempIndex.ToString();
+
+    }
+
+    private void Start()
+    {
+        CheckLockStatus();
     }
 
     private void SetLevelIndex()
@@ -27,5 +36,31 @@ public class LevelBtn : MonoBehaviour
         PlayerPrefs.SetInt(ConstantKeys.LEVEL_INDEX, levelIndex);
         levelData.currentLevel = levelIndex;
         SceneManager.LoadScene(1);
+    }
+
+    private void CheckLockStatus()
+    {
+        int currentLockStatus = PlayerPrefs.GetInt(ConstantKeys.LEVEL_UNLOCKED, 0);
+        Debug.Log("Unlocked level: " + currentLockStatus);
+
+        if (currentLockStatus >= levelIndex)
+        {
+            myButton.interactable = true;
+            lockImage.SetActive(false);
+            
+            if (currentLockStatus == levelIndex)
+            {
+                currentLevelImage.SetActive(true);
+                float randomRot = Random.Range(-24f, 97f);
+                currentLevelImage.transform.rotation = Quaternion.Euler(0, 0, randomRot);
+            }
+            else
+                currentLevelImage.SetActive(false);
+        }
+        else
+        {
+            myButton.interactable = false;
+            lockImage.SetActive(true);
+        }
     }
 }
